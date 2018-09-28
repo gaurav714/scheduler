5d00f7b297bb3227a7b329dcb16ce65bd4464375
gaurav714 <gauravnautiyal77@gmail.com>
2018-09-28 21:55:37 +0530

    shorest kafka with scheduler ever

--- src/main/java/com/stackroue/buzzup/schedular/Message.java
+++ src/main/java/com/stackroue/buzzup/schedular/Message.java
@@ -1,32 +1,38 @@
 package com.stackroue.buzzup.schedular;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
+
 
 
 
 public class Message implements Job {
 
 	private String BOOT_TOPIC = "booking";
-	private KafkaTemplate<String, String> kafkaTemplate;
-	
-	@Autowired
-	public Message(KafkaTemplate<String, String> kafkaTemplate) {
-		this.kafkaTemplate = kafkaTemplate;
-	}
+
+
 	
 	public void execute(JobExecutionContext context)
 		throws JobExecutionException {
+		Properties props = new Properties();
+		 props.put("bootstrap.servers", "127.0.0.1:9092");
+		 props.put("acks", "all");
+		 props.put("retries", 0);
+		 props.put("batch.size", 16384);
+		 props.put("linger.ms", 1);
+		 props.put("buffer.memory", 33554432);
+		 props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		 props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		 Producer<String, String> producer = new KafkaProducer<>(props);
+		 producer.send(new ProducerRecord<String, String>(BOOT_TOPIC,"booking closed"));
+		
 	
-		try {
-		kafkaTemplate.send(BOOT_TOPIC, "booking closed");
-		}
-		catch(Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
 		
 		System.out.println("booking closed");
 	}
